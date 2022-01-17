ebd91f01e9359c171bf8f19edea5d3923df0f175
Cristian de Gea <cgea@aidacanarias.com>
2022-01-17 23:59:37 +0100

    ✅ generate_a_golden_master()

--- C#/Trivia/Trivia.Tests/GameTest.cs
+++ C#/Trivia/Trivia.Tests/GameTest.cs
@@ -1,12 +1,25 @@
+using System;
+using System.IO;
 using Xunit;
 
 namespace Trivia.Tests
 {
     public class GameTest
     {
-        [Fact]
-        public void Test1()
+        [Fact (Skip = "true")]
+        public void generate_a_golden_master()
         {
+            var path = "../../../GoldenMasterOutputs";
+            Directory.CreateDirectory(path);
+
+            for (var i = 0; i <= 200; i++)
+            {
+                var fileName = $"gameOutput{i}.txt";
+                using var fileStream = new FileStream(Path.Combine(path, fileName), FileMode.Create);
+                using var outputStream = new StreamWriter(fileStream);
+                Console.SetOut(outputStream);
+                GameRunner.Main(new [] {i.ToString()});
+            }
         }
     }
 }
